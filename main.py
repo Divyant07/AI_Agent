7928ffbdfb4bd93c00b2c774e62a11c4b1fd9f80
Divyant M <divyantm1@gmail.com>
2025-03-15 12:40:07 +0530

    dotenv syntax error, created response var

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 
-load_dotenv
+load_dotenv()
 
 llm = ChatOpenAI(model="gpt-4o-mini")
+response = llm.invoke("Who is Illaoi in League of Legends?")
+print(response)
